680a3e863d2815be3a6789093119346153f7a738
fatihkarasoglu <f.karasoglu.01@gmail.com>
2023-05-20 06:28:40 +0300

    commit-3

--- src/App.js
+++ src/App.js
@@ -1,76 +1,25 @@
-import { useEffect, useState } from 'react';
+import { PostService, UserService } from './services/';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-  const [name, setName] = useState('Fatih');
+  // useEffect(() => {
+  //   PostService.getPost().then(res => console.log(res))
+  //   PostService.getPostDetails(2).then(res => console.log(res))
+  //   PostService.newPost({
+  //     userId: 3,
+  //     title: 'test',
+  //     body: 'test'
+  //   }).then(res => console.log(res))
 
-  const [avatar, setAvatar] = useState(false)
-
-  const [users, setUsers] = useState(false);
-
-  const addPost = data => {
-
-    const headers = new Headers();
-    // headers.append('Content-type', 'application/json')
-    headers.append('Authorization', 'Bearer Fatih8645315684')
-
-    const formData = new FormData();
-    formData.append('userId', data.userId)
-    formData.append('title', data.title)
-    formData.append('body', data.body)
-
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      // body: JSON.stringify(data),
-      body: formData,
-      headers
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-  }
-  
-  useEffect(() => {
-
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => {
-      if(res.ok && res.status === 200) {
-        return res.json();
-      }
-    })
-    .then(data => setUsers(data))
-    .catch(err => console.log(err))
-
-    addPost({
-      userId: 1,
-      title: 'Örnek Post',
-      body: 'Post İçeriği'
-    })
-
-  }, [])
-
-  const SubmitHandle = e => {
-    e.preventDefault();
-  }
+  //   UserService.getUsers();
+  // })
 
   return (
-    <>
-      <form onSubmit={SubmitHandle}>
-        <input type='text' name='name' value={name} onChange={e => setName(e.target.value)} /> <br />
-        <input type='file' name='avatar' onChange={e => setAvatar(e.target.files[0])} /> <br />
-        <button type='submit' disabled={!name || !avatar}>Kaydet</button>
-      </form>
-
-      <h1>User List</h1>
-      <ul>
-        {users && users.map(user => (
-          <li key={user.id}>
-            {user.name}
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className='App'>
+      Merhaba
+    </div>
   );
 }
 
---
+++ src/services/post.js
@@ -0,0 +1,5 @@
+// import { post, get, postJSON } from "./request";
+
+// export const getPost = () => get('posts');
+// export const getPostDetails = id => get(`posts/${id}`);
+// export const newPost = data => postJSON(`posts`, data);
