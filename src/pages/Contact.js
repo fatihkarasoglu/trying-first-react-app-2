089df19db7594f709676c6ccde67541ced6925d5
fatihkarasoglu <f.karasoglu.01@gmail.com>
2023-05-27 02:22:58 +0300

    commit-6.1

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,4 @@
+import { Formik, Form, Field } from "formik"
 import { Helmet } from "react-helmet"
 
 export default function Contact() {
@@ -7,7 +8,43 @@ export default function Contact() {
             <Helmet>
                 <title>İletişim</title>
             </Helmet>
-            Contact page
+            <h3>İletişim</h3> <br />
+
+            <Formik 
+                initialValues={{
+                    name: 'Fatih',
+                    accept: false,
+                    about: '',
+                    gender: '',
+                    skills: [],
+                    avatar: ''
+                }} 
+                onSubmit={values => {
+                    console.log(values);
+                }}>
+                {({ values }) => (
+                    <Form>
+                        <Field name="name" /> <br />
+                        <Field component="textarea" name="about" /> <br />
+                        <label>
+                            <Field type="checkbox" name="accept" />
+                            Kuralları kabul ediyorum.
+                        </label> <br />
+                        <Field component="select" name="gender">
+                            <option value={1}>Kadın</option>
+                            <option value={2}>Erkek</option>
+                        </Field>
+                        <Field component="select" name="skills" multiple={true}>
+                            <option value="React">React</option>
+                            <option value="Node.js">Node.js</option>
+                            <option value="Vue.js">Vue.js</option>
+                            <option value="Javascript">Javascript</option>
+                        </Field>
+                        <Field type="file" name="avatar" />
+                        <button disabled={!values.accept} type="submit">Gönder</button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 }
--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -22,20 +22,6 @@ export default function Login() {
         })
     }
 
-    const { handleSubmit, handleChange, values } = useFormik({
-        initialValues: {
-            username: '',
-            email: '',
-            password: ''
-        },
-        onSubmit: values => {
-            setUser(values)
-            navigate(location?.state?.return_url || '/', {
-                replace: true
-            })
-        }
-    });
-
     return(
         <div>
             <Helmet>
@@ -43,19 +29,27 @@ export default function Login() {
             </Helmet>
             Login page <br />
 
-            <hr />
-
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Kullanıcı Adı</label>
-                <input type="text" id="username" value={values.username} onChange={handleChange} />   <br />
-
-                <label htmlFor="email">E-posta</label>
-                <input type="email" id="email" value={values.email} onChange={handleChange} />  <br />
-
-                <label htmlFor="password">Parola</label>
-                <input type="password" id="password" value={values.password} onChange={handleChange} />
-                <button type="submit">Giriş Yap</button>
-            </form>
+            <Formik 
+                initialValues={{
+                    username: '',
+                    email: '',
+                    password: ''
+                }} 
+                onSubmit={values => {
+                    setUser(values)
+                    navigate(location?.state?.return_url || '/', {
+                    replace: true
+                })
+            }}>
+                {props => (
+                    <Form>
+                        <Field name="username" /> <br />
+                        <Field name="email" /> <br />
+                        <Field name="password" /> <br />
+                        <button type="submit">Giriş Yap</button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 }
